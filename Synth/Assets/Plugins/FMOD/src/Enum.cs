43542c424f6159672ed0584a53a44647cab0f104
Dohoon Kim <77309555+UeharaKaro@users.noreply.github.com>
2025-08-05 03:44:51 +0900

    last
    잠들기전 최고의 선택

---
+++ Synth/Assets/Plugins/FMOD/src/Enum.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMOD;
+
+// SFX(효과음)을 정의하는 Enum(열거형), 숫자대신 의미있는 이름 사용가능
+public enum SFXType
+{
+    Metronome, //메트로놈 소리(쉼표 유무는 선택
+    Hit, 
+    
+}
