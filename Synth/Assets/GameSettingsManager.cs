e66ed6d67bd5de7bae63f6b7a9c47ff3fb1554df
Dohoon Kim <77309555+UeharaKaro@users.noreply.github.com>
2025-08-06 12:46:31 +0900

    오탈자 수정

--- Synth/Assets/GameSettingsManager.cs
+++ Synth/Assets/GameSettingsManager.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
-public class GameSettingManager : MonoBehaviour
+public class GameSettingsManager : MonoBehaviour
 {
-    // 다른 스크립트에서 GameSettingManager.Instance 로 쉽게 접근할 수 있도록 설정
-    public static GameSettingManager Instance { get; private set; }
+    // 다른 스크립트에서 GameSettingsManager.Instance 로 쉽게 접근할 수 있도록 설정
+    public static GameSettingsManager Instance { get; private set; }
 
     // 현재 선택된 판정 모드. 기본값은 Normal
     public JudgmentMode CurrentMode { get; set; } = JudgmentMode.JudgmentMode_Normal;
@@ -18,7 +18,7 @@ public class GameSettingManager : MonoBehaviour
             // 이 객체를 파괴하고, 기존의 것을 계속 사용
             // 이렇게 하면 씬을 다시 로드해도 설정 유지 가능
             Destroy(gameObject);
-            return;
+            return; 
         }
 
         // Instance가 없다면, 이 객체를 Instance로 지정
