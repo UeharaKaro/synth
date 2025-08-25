7e3a6161f100f764809cbf4d552ba23ded9eab67
Dohoon Kim <77309555+UeharaKaro@users.noreply.github.com>
2025-08-25 20:00:38 +0900

    키설정

---
+++ Synth/Assets/SongSelectionManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro; // TextMeshPro를 사용하기 위해 필요 
+
+
+public class SongSelectionManager : MonoBehaviour   
+{
+    
+}
